1b7f6517e825a236f034955faa013b4ed8291f1c
Willian Ribeiro <57929088+WillianVMR@users.noreply.github.com>
2024-12-17 21:12:57 -0300

    terraform scripts to deploy azure function

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+  os_type = "Linux"
+}
+
+resource "azurerm_function_app" "example" {
+  name                       = "example-function-app"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  version                    = "~3"
+  os_type                    = "linux"
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "python"
+    AzureWebJobsStorage      = azurerm_storage_account.example.primary_connection_string
+  }
+} 
